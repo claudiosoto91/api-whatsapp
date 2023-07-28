9553c669e6052fda431b0cc5fb860f8fe46dd556
Claudio Soto <claudio91soto@gmail.com>
2023-07-28 01:14:37 -0300

    6

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -6,10 +6,12 @@ const verificar = ( req, res ) => {
 
         if ( challenge != null && token!= null && token == tokenprueba ) {
             res.send(challenge);
+        }else{
+            res.status(400).send();
         }
         console.log(req);
     } catch (e) {
-        res.status(404).send();
+        res.status(400).send();
     }
     res.send("Verificado");
 }
