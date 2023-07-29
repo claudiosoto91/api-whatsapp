2e6ab1a6a0b40958643c89c2a72160a7296f9fd2
Claudio Soto <claudio91soto@gmail.com>
2023-07-28 22:33:57 -0300

    corrigiendo reading 0

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,45 +1,45 @@
 const enviarMensaje = require("../service/apiservice");
 
 const verificar = (req, res) => {
-    try {
-        var tokenprueba = "TOKENPRUEBA";
-        var token = req.query["hub.verify_token"];
-        var challenge = req.query["hub.challenge"];
+  try {
+    var tokenprueba = "TOKENPRUEBA";
+    var token = req.query["hub.verify_token"];
+    var challenge = req.query["hub.challenge"];
 
-        if (challenge != null && token != null && token == tokenprueba) {
-            res.send(challenge);
-        } else {
-            res.status(400).send();
-        }
-    } catch (e) {
-        res.status(400).send();
+    if (challenge != null && token != null && token == tokenprueba) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
     }
-}
+  } catch (e) {
+    res.status(400).send();
+  }
+};
 
 const recibir = (req, res) => {
+  try {
+    var entry = req.body["entry"][0];
+    var changes = entry["changes"][0];
+    var value = changes["value"];
+    var objetoMensaje = value["messages"];
 
-    try {
-        var entry = (req.body["entry"])[0];
-        var changes = (entry["changes"])[0];
-        var value = changes["value"];
-        var objetoMensaje = value["messages"];
-        // console.log(objetoMensaje);
+    if (typeof objetoMensaje !== "undefined") {
+      var messages = objetoMensaje[0];
+      var texto = messages["text"]["body"];
+      var number = messages["from"];
 
-        var messages = objetoMensaje[0];
-        var texto = messages["text"]["body"];
-        var number = messages["from"];
-
-        console.log("Enviado desde : "+ number + " El texto es: "+ texto);
-        enviarMensaje.EnviarMensajeWhatsApp(texto, number);
-
-        res.send("EVENT_RECEIVED");
-    } catch (e) {
-        console.log(e);
-        res.send("EVENT_RECEIVED");
+      console.log("Enviado desde : " + number + " El texto es: " + texto);
+      enviarMensaje.EnviarMensajeWhatsApp(texto, number);
     }
-}
+
+    res.send("EVENT_RECEIVED");
+  } catch (e) {
+    console.log(e);
+    res.send("EVENT_RECEIVED");
+  }
+};
 
 module.exports = {
-    verificar,
-    recibir
+  verificar,
+  recibir,
 };
